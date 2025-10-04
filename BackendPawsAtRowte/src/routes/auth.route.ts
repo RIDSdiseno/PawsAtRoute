1e1908651b771409fe3c6a8af5dffd350e92a151
Mario230703 <ma.benedetti@duocuc.cl>
2025-10-04 20:00:44 -0300

    funcion de recuperar contraseña

--- BackendPawsAtRowte/src/routes/auth.route.ts
+++ BackendPawsAtRowte/src/routes/auth.route.ts
@@ -1,14 +1,22 @@
 import { Router } from "express";
-import { login, registerUser, refresh, getProfile, logout } from "../controllers/auth.controller";
+import { login, registerUser, refresh, getProfile, logout, sendVerificationCode, verifyCode, resetPassword } from "../controllers/auth.controller";
 import { authGuard } from "../middlewares/auth.middleware";
 
 const r = Router();
 
-r.get("/health", (_req, res) => res.json({ ok: true, service: "API Movil", ts: Date.now() }));
+r.get("/health", (_req, res) => res.json({ ok: true, service: "API Paws", ts: Date.now() }));
 
-r.post("/register",registerUser)
-r.post("/login",login)
-r.post("/refresh",authGuard,refresh)
-r.post("/logout",authGuard,logout)
+r.post("/register",registerUser);
+r.post("/login",login);
+r.post("/refresh",authGuard,refresh);
+r.post("/logout",authGuard,logout);
 r.get("/profile", authGuard, getProfile);
+
+
+r.post("/send-code",sendVerificationCode);
+r.post("/verify-code",verifyCode);
+r.put("/reset-password",resetPassword);
+
+
+
 export default r;
