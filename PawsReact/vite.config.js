6a7146fc7e28979bb628e485d544b8f2de5a764f
Mario230703 <ma.benedetti@duocuc.cl>
2025-10-05 11:39:52 -0300

    a

--- PawsReact/vite.config.js
+++ PawsReact/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   server:{
-    historycApiFallback: true,
+    historyApiFallback: true,
   },
   plugins: [react(),tailwindcss()],
   build:{
