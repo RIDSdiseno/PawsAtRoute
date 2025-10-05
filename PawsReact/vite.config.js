e60a1cc262a22f4f604081f1accb61d1abd1147f
Mario230703 <ma.benedetti@duocuc.cl>
2025-10-05 12:39:33 -0300

    a

--- PawsReact/vite.config.js
+++ PawsReact/vite.config.js
@@ -8,6 +8,12 @@ export default defineConfig({
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 5173, // Render inyecta PORT
     strictPort: true,                       // falla si no puede usar ese puerto
+    allowedHosts: ['pawsatroute1.onrender.com'],
+    hmr: {
+      host: 'pawsatroute1.onrender.com',
+      protocol: 'wss',
+      clientPort: 443,
+    },               
   },
   build: { outDir: 'dist' },
   publicDir: 'public',
