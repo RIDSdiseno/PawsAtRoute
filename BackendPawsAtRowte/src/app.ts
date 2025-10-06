49a7f6e079cd8dd8492b465fa952b501d4b84263
Mario230703 <ma.benedetti@duocuc.cl>
2025-10-06 11:28:01 -0300

    Arreglo multer

--- BackendPawsAtRowte/src/app.ts
+++ BackendPawsAtRowte/src/app.ts
@@ -1,4 +1,6 @@
 // src/app.ts
+import fs from "fs";
+import path from "path";
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
@@ -8,15 +10,25 @@ import { env } from './config/env.js';
 import { errorHandler } from './middlewares/error.middleware.js';
 export const app = express();
 
+const up = path.resolve(process.cwd(), "uploads");
+if (!fs.existsSync(up)) fs.mkdirSync(up, { recursive: true });
+
+
 app.use(cors({
     origin: '*',
     methods: ['GET','POST','PUT','DELETE'],
     allowedHeaders: ['Content-Type','Authorization']
 }));
 
+app.use("/uploads", express.static(up));
+
+
 app.use(cookieParser());                             // 👈 DEBE ir antes de las rutas
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use('/api', routes);    
 // debug opcional de cookies:
